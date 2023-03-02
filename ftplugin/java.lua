fa396449c383d9755e610466adabc7aee92bfe47
gdtknight <gdtknight@naver.com>
2023-03-02 14:52:49 +0000

    format java.lua

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -40,158 +40,159 @@ local bundles = {}
 local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
 vim.list_extend(bundles, vim.split(vim.fn.glob(mason_path .. "packages/java-test/extension/server/*.jar"), "\n"))
 vim.list_extend(
-    bundles,
-    vim.split(
-        vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"),
-        "\n"
-    )
+  bundles,
+  vim.split(
+    vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"),
+    "\n"
+  )
 )
 
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
-    -- The command that starts the language server
-    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-    cmd = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
 
-        -- 💀
-        "java", -- or '/path/to/java11_or_newer/bin/java'
-        -- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
-        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-        "-Dosgi.bundles.defaultStartLevel=4",
-        "-Declipse.product=org.eclipse.jdt.ls.core.product",
-        "-Dlog.protocol=true",
-        "-Dlog.level=ALL",
-        "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-        "-Xms1g",
-        "--add-modules=ALL-SYSTEM",
-        "--add-opens",
-        "java.base/java.util=ALL-UNNAMED",
-        "--add-opens",
-        "java.base/java.lang=ALL-UNNAMED",
-
-        -- 💀
-        "-jar",
-        vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-        -- Must point to the                                                     Change this to
-        -- eclipse.jdt.ls installation                                           the actual version
-
-        -- 💀
-        "-configuration",
-        home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. CONFIG,
-        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-        -- Must point to the                      Change to one of `linux`, `win` or `mac`
-        -- eclipse.jdt.ls installation            Depending on your system.
-
-        -- 💀
-        -- See `data directory configuration` section in the README
-        "-data",
-        workspace_dir,
-    },
-    -- on_attach = require("lvim.lsp").on_attach,
-    capabilities = capabilities,
     -- 💀
-    -- This is the default if not provided, you can remove it. Or adjust as needed.
-    -- One dedicated LSP server & client will be started per unique root_dir
-    root_dir = root_dir,
-    -- Here you can configure eclipse.jdt.ls specific settings
-    -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-    -- or https://github.com/redhat-developer/vscode-java#supported-vs-code-settings
-    -- for a list of options
-    settings = {
-        java = {
-            -- jdt = {
-            --   ls = {
-            --     vmargs = "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m"
-            --   }
-            -- },
-            eclipse = {
-                downloadSources = true,
-            },
-            configuration = {
-                updateBuildConfiguration = "interactive",
-                runtimes = {
-                    {
-                        name = "JavaSE-11",
-                        path = "~/.sdkman/candidates/java/11.0.18-tem",
-                    },
-                    {
-                        name = "JavaSE-19",
-                        path = "~/.sdkman/candidates/java/19.0.2-tem",
-                    },
-                },
-            },
-            maven = {
-                downloadSources = true,
-            },
-            implementationsCodeLens = {
-                enabled = false,
-            },
-            referencesCodeLens = {
-                enabled = false,
-            },
-            references = {
-                includeDecompiledSources = true,
-            },
-            inlayHints = {
-                parameterNames = {
-                    enabled = "all", -- literals, all, none
-                },
-            },
-            -- format = {
-            -- 	enabled = true,
-            -- 	settings = {
-            -- 	  profile ="GoogleStyle",
-            --        url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
-            -- 	}
-            -- },
+    "java", -- or '/path/to/java11_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+    "-Xms256m",
+    "-Xmx768m",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+
+    -- 💀
+    "-jar",
+    vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+    -- Must point to the                                                     Change this to
+    -- eclipse.jdt.ls installation                                           the actual version
+
+    -- 💀
+    "-configuration",
+    home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. CONFIG,
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+    -- eclipse.jdt.ls installation            Depending on your system.
+
+    -- 💀
+    -- See `data directory configuration` section in the README
+    "-data",
+    workspace_dir,
+  },
+  -- on_attach = require("lvim.lsp").on_attach,
+  capabilities = capabilities,
+  -- 💀
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = root_dir,
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- or https://github.com/redhat-developer/vscode-java#supported-vs-code-settings
+  -- for a list of options
+  settings = {
+    java = {
+      -- jdt = {
+      --   ls = {
+      --     vmargs = "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m"
+      --   }
+      -- },
+      eclipse = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        runtimes = {
+          {
+            name = "JavaSE-11",
+            path = "~/.sdkman/candidates/java/11.0.18-tem",
+          },
+          {
+            name = "JavaSE-19",
+            path = "~/.sdkman/candidates/java/19.0.2-tem",
+          },
         },
-        signatureHelp = { enabled = true },
-        completion = {
-            favoriteStaticMembers = {
-                "org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*",
-                "org.springframework.test.web.servlet.result.MockMvcResultHandlers.*",
-                "org.springframework.test.web.servlet.result.MockMvcResultMatchers.*",
-                "org.assertj.core.api.Assertions.*",
-                "org.mockito.Mockito.*",
-                "org.mockito.ArgumentMatchers.*",
-                "org.mockito.BDDMockito.*",
-                "org.junit.jupiter.api.Assertions.*",
-                "org.junit.jupiter.params.provider.Arguments.*",
-                "java.util.Objects.requireNonNull",
-                "java.util.Objects.requireNonNullElse",
-            },
+      },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = false,
+      },
+      referencesCodeLens = {
+        enabled = false,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = "all", -- literals, all, none
         },
-        contentProvider = { preferred = "fernflower" },
-        extendedClientCapabilities = extendedClientCapabilities,
-        sources = {
-            organizeImports = {
-                starThreshold = 9999,
-                staticStarThreshold = 9999,
-            },
-        },
-        codeGeneration = {
-            toString = {
-                template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-            },
-            useBlocks = true,
-        },
+      },
+      -- format = {
+      -- 	enabled = true,
+      -- 	settings = {
+      -- 	  profile ="GoogleStyle",
+      --        url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+      -- 	}
+      -- },
     },
-    flags = {
-        allow_incremental_sync = true,
+    signatureHelp = { enabled = true },
+    completion = {
+      favoriteStaticMembers = {
+        "org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*",
+        "org.springframework.test.web.servlet.result.MockMvcResultHandlers.*",
+        "org.springframework.test.web.servlet.result.MockMvcResultMatchers.*",
+        "org.assertj.core.api.Assertions.*",
+        "org.mockito.Mockito.*",
+        "org.mockito.ArgumentMatchers.*",
+        "org.mockito.BDDMockito.*",
+        "org.junit.jupiter.api.Assertions.*",
+        "org.junit.jupiter.params.provider.Arguments.*",
+        "java.util.Objects.requireNonNull",
+        "java.util.Objects.requireNonNullElse",
+      },
     },
-    -- Language server `initializationOptions`
-    -- You need to extend the `bundles` with paths to jar files
-    -- if you want to use additional eclipse.jdt.ls plugins.
-    --
-    -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-    --
-    -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-    init_options = {
-        -- bundles = {},
-        bundles = bundles,
+    contentProvider = { preferred = "fernflower" },
+    extendedClientCapabilities = extendedClientCapabilities,
+    sources = {
+      organizeImports = {
+        starThreshold = 9999,
+        staticStarThreshold = 9999,
+      },
     },
+    codeGeneration = {
+      toString = {
+        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+      },
+      useBlocks = true,
+    },
+  },
+  flags = {
+    allow_incremental_sync = true,
+  },
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    -- bundles = {},
+    bundles = bundles,
+  },
 }
 
 config["on_attach"] = function(client, bufnr)
@@ -220,10 +221,10 @@ config["on_attach"] = function(client, bufnr)
 end
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "*.java" },
-    callback = function()
-      local _, _ = pcall(vim.lsp.codelens.refresh)
-    end,
+  pattern = { "*.java" },
+  callback = function()
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+  end,
 })
 
 -- This starts a new client & server,
@@ -237,5 +238,3 @@ vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_co
 vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
 vim.cmd "command! -buffer JdtBuildProject lua require('jdtls').build_projects()"
 -- vim.cmd "command! -buffer JdtJshell lua require('jdtls').jshell()"
-
-
